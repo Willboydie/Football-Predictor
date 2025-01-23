64e407bdc5ba650cd026e7c31e0bbc188f586d33
Willboydie <williamteboyd@gmail.com>
2025-01-23 20:26:50 +0000

    initial set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import footballLogo from '/footballLogo.svg'
+import {useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [likes, setLikes] = useState(0);
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={footballLogo} className="logo" alt="footballLogo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Football Predictor</h1>
+      <div>
+        <h2>
+          "Football is a {(likes > 11) ? 11 : likes}/10 sport!"
+        </h2>
+        <button onClick={() => setLikes((likes) => likes + 1)}>
+          Click to tell us how much you like football!
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
